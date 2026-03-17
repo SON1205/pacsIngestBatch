714ec072b1e94c2fbb94eee82d45d8a4b906fa05
SON1205 <jjinoson1205@gmail.com>
2026-03-17 17:50:45 +0900

    init

---
+++ src/main/java/com/planitsquare/medingestex/MedIngestExApplication.java
@@ -0,0 +1,13 @@
+package com.planitsquare.medingestex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedIngestExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedIngestExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/planitsquare/medingestex/MedIngestExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planitsquare.medingestex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedIngestExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
